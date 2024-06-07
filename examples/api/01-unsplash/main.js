ff2741d646732a91a6a0849f0fb3f809469e2603
Jonas <git@jonasscheiwiller.ch>
2024-06-07 11:31:48 +0200

    feat: change unsplash example and log response as well

--- examples/api/01-unsplash/main.js
+++ examples/api/01-unsplash/main.js
@@ -1,21 +1,26 @@
-fetch('https://api.unsplash.com/photos/random', {
+fetch("https://api.unsplash.com/photos/random", {
   headers: {
-    Authorization: `Client-ID ${UNSPLASH_ACCESS_KEY}`
-  }
+    Authorization: `Client-ID ${UNSPLASH_ACCESS_KEY}`,
+  },
 })
-  .then(response => response.json())
-  .then(data => {
+  .then((response) => {
+    console.log("response: ", response);
+    return response.json();
+  })
+  .then((data) => {
     // Process the fetched data here
     console.log(data);
 
     // display photo to the screen
-    const img = document.createElement('img');
+    const img = document.createElement("img");
     img.src = data.urls.regular;
     img.alt = data.slug;
+
     const photoDiv = document.querySelector('[data-js="photo"]');
+
     photoDiv.appendChild(img);
   })
-  .catch(error => {
+  .catch((error) => {
     // Handle any errors that occur during the fetch request
-    console.error('Error:', error);
+    console.error("Error:", error);
   });
